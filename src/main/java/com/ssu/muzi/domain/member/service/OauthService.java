b48b51e909dfbce3a5b3a0292c067f761c197e29
김혜령 <hyeryoung27@soongsil.ac.kr>
2025-03-07 02:30:46 +0900

    fix: 오류 수정

--- src/main/java/com/ssu/muzi/domain/member/service/OauthService.java
+++ src/main/java/com/ssu/muzi/domain/member/service/OauthService.java
@@ -12,7 +12,5 @@ public interface OauthService {
     String getTokens(Long id, HttpServletResponse response);
     String loginWithKakao(String accessToken, HttpServletResponse response);
     OauthResponse.RefreshTokenResponse tokenRefresh(HttpServletRequest request);
-    OauthResponse.KakaoInfo getMyInfo();
-    Member findMemberByAuthId(Long authId);
     OauthResponse.CheckMemberRegistration checkRegistration(OauthRequest.LoginRequest request);
 }
