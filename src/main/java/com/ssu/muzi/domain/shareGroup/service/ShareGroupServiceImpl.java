e92737b7eaac5c797407acb575a9c7e4a7f0ef3c
김혜령 <hyeryoung27@soongsil.ac.kr>
2025-04-25 17:47:54 +0900

    fix: 홈 화면 API 수정

--- src/main/java/com/ssu/muzi/domain/shareGroup/service/ShareGroupServiceImpl.java
+++ src/main/java/com/ssu/muzi/domain/shareGroup/service/ShareGroupServiceImpl.java
@@ -197,7 +197,7 @@ public class ShareGroupServiceImpl implements ShareGroupService {
                 .stream()
                 .map(group -> {
                     Status status = computeGroupStatus(group, now);
-                    return status != null ? shareGroupConverter.toHomeDetail(group, status) : null;
+                    return status != Status.FINAL_ENDED ? shareGroupConverter.toHomeDetail(group, status) : null;
                 })
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
